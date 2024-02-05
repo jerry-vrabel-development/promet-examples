6ccc513c32795339a9a7281ce315eff718414871
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2024-02-05 15:31:49 -0600

    *WIP webpack and react add

---
+++ web/themes/custom/codereview/js/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// "Hello, World" code
+ReactDOM.render(
+  <h1>Hello there - world!</h1>,
+  document.getElementById('react-app')
+);
